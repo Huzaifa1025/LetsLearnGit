6409a74bded60840d55f7216429e05a4c263155e
Huzaifa Manasawala <huzzichamp1025@gmail.com>
2023-05-28 16:40:11 +0530

    added in divide.cpp

---
+++ divide.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int number1,number2;
+    float div;
+    cout<<"Enter First number :";
+    cin>>number1;
+    cout<<"Enter second number :";
+    cin>>number2;
+    div=number1/number2;
+    cout<<" Division of two numbers :"<<div<<endl;
+    return 0;
+}
