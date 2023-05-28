cc6ea7af916ce4a8fcc955070726fb2ce0e1528b
Huzaifa Manasawala <huzzichamp1025@gmail.com>
2023-05-29 01:47:05 +0530

    added statement in mul.cpp

--- mul.cpp
+++ mul.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main()
 {
-    int n,a;
+    int n,a,mul;
     cout << "enter first num";
     cin>>n;
     cout << "enter second num";
     cin>>a;
-
+    mul = a*n;
+    cout << "the product of the two num is "<<mul;
     return 0;
 }
 
